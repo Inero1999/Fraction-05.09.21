8042c9c90e78faceb679e15edc414a989aa9724d
Inero1999 <eak1972@mail.ru>
2021-09-05 12:09:45 +0300

    Реализация класса дробь. Неполная.

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Fraction
+{
+    public class Fraction
+    {
+        public int _chisl { get; set; }
+        public int _znam { get; set; }
+
+        public Fraction(int _chisl, int _znam)
+        {
+            this._chisl = _chisl;
+            this._znam = _znam;
+
+        }
+        public static bool operator true(Fraction f)
+        {
+            return f._chisl < f._znam ? true : false;
+        }
+        public static bool operator false(Fraction f)
+        {
+            return f._chisl >= f._znam ? true : false;
+        }
+        public static Fraction operator *(Fraction f, int a)
+        {
+
+            return new Fraction(f._chisl * a, f._znam);
+        }
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        {
+
+            Fraction f = new Fraction(f1._chisl * f2._chisl, f1._znam * f2._znam);
+            if (f)
+            {
+                WriteLine("Дробь правильная");
+                return f;
+                
+            }
+            else
+            {
+                WriteLine("Дробь неправильная");
+                return f;
+                
+            }
+        }
+        public static Fraction operator/(Fraction f1, Fraction f2)
+        {
+
+            Fraction f = new Fraction(f1._chisl * f2._znam, f1._znam * f2._chisl);
+            if (f)
+            {
+                WriteLine("Дробь правильная");
+                return f;
+
+            }
+            else
+            {
+                WriteLine("Дробь неправильная");
+                return f;
+
+            }
+        }
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+
+            Fraction f = new Fraction(f1._chisl * f2._znam+f2._chisl*f1._znam, f1._znam * f2._znam);
+            if (f)
+            {
+                WriteLine("Дробь правильная");
+                return f;
+
+            }
+            else
+            {
+                WriteLine("Дробь неправильная");
+                return f;
+
+            }
+        }
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+
+            Fraction f = new Fraction(f1._chisl * f2._znam - f2._chisl * f1._znam, f1._znam * f2._znam);
+            if (f)
+            {
+                WriteLine("Дробь правильная");
+                return f;
+
+            }
+            else
+            {
+                WriteLine("Дробь неправильная");
+                return f;
+
+            }
+        }
+
+        //переопределение метода Equals
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            Fraction f = obj as Fraction; // возвращает null если объект нельзя привести к типу Fraction
+            if (f as Fraction == null)
+                return false;
+
+            return f._chisl == this._chisl && f._znam == this._znam;
+            
+        }
+        // необходимо также переопределить метод 
+        // GetHashCode
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        public static bool operator ==(Fraction f1, Fraction f2)
+        {
+           
+            return f1.Equals(f2);
+        }
+        public static bool operator !=(Fraction f1, Fraction f2)
+        {
+            
+            return !(f1==f2);
+        }
+
+        public static bool operator >(Fraction f1, Fraction f2)
+        {
+
+            return (f1._chisl*f2._znam > f2._chisl*f1._znam);
+        }
+        public static bool operator<(Fraction f1, Fraction f2)
+        {
+
+            return (f1._chisl * f2._znam < f2._chisl * f1._znam);
+        }   
+       
+
+
+
+        //public void Reduction()
+        //{
+        //    int a = Math.Abs(_chisl);
+        //    int b = Math.Abs(_znam);
+        //    int c = 1;
+
+        //    while (c > 0)
+        //    {
+        //        c = b % a;
+        //        a = b;
+        //        b = c;
+        //    }
+        //    int nok = a * _chisl * _znam;
+        //    // Находим НОД
+        //    if (nok != 1)
+        //    {
+        //        _chisl = _chisl / nok;
+        //        _znam = _znam / nok;
+        //    }
+
+        //}
+        public void Print()
+        {
+            WriteLine($"{_chisl}/{_znam}");
+           
+        }
+    }
+}
---
+++ Fraction/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Fraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 8;
+            Fraction f1 = new Fraction(3,4);
+            Fraction f2 = new Fraction(2,3);
+            f1.Print();
+            
+            Fraction f3 = f1 * a;
+            f3.Print();
+            
+            Fraction f4 = f1 +f2;
+            f4.Print();
+
+            Fraction f5 = f1 - f2;
+            f5.Print();
+
+            if (f1 == f2) 
+            {
+                WriteLine("Дроби равны");
+            }
+            else
+            {
+                WriteLine("Дроби не равны");
+            }
+
+            if (f1 > f2)
+            {
+                WriteLine($"Дробь {f1._chisl}/{f1._znam} больше дроби {f2._chisl}/{f2._znam}");
+            }
+            else
+            {
+                WriteLine($"Дробь {f1._chisl}/{f1._znam} меньше дроби {f2._chisl}/{f2._znam}");
+            }
+
+        }
+    }
+}
